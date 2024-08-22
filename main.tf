2b6b0db0ebfe7da7ff5e0eba45d5564dac8e6242
ramya <ramyaakula457@gmail.com>
2024-08-22 11:16:22 +0530

    file updated

--- main.tf
+++ main.tf
@@ -4,7 +4,7 @@ provider "aws" {
 }
 resource "aws_instance" "ramya" {
     ami = "ami-02b49a24cfb95941c"
-    instance_type = "t2.micro"
+    instance_type = "t2.nano"
     key_name = "devsecops"
     tags = {
       Name = "ramya-dev"
